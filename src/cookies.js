fc8f761ed7fcc03b04b612f589930ad229563558
Andrea Giammarchi <gimmarch@Andrea-Giammarchi.local>
2012-04-09 18:19:01 +0200

    renamed cookies.js into cookie.js

--- src/cookies.js
+++
@@ -1,7 +0,0 @@
-// cookie version ... but JC change target browsers!
-function AsynchronousStorage(name) {
-  this.name = name;
-  this.type = "cookies";
-};
-
-asPrototype = AsynchronousStorage[prototype];
